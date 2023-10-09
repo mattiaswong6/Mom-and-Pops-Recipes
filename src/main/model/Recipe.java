890c81e38b056f891081d5ec4d89d14a91b6c0f3
Mattias Wong <mwong54@student.ubc.ca>
2023-10-08 21:02:57 -0700

    Tested and implemented constructor, addIngredientToRecipe, and getters for Recipe class.

--- src/main/model/Recipe.java
+++ src/main/model/Recipe.java
@@ -1,37 +1,47 @@
 package model;
 
-//import java.util.ArrayList;
-//import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Recipe {
-//    private String name;
-//    private List<Ingredient> ingredientsNeeded;
-//    private int prepTime;
-//
-//    // REQUIRES: recipeName has a non-zero length, time > 0.
-//    // EFFECTS: creates a new Recipe with a given name and prep time (in minutes),
-//    // and an empty list of ingredients required.
-//    public Recipe(String recipeName, int time) {
-//        this.name = recipeName;
-//        this.prepTime = time;
-//        this.ingredientsNeeded = new ArrayList<>();
-//    }
-//
-//    // MODIFIES: this
-//    // EFFECTS: adds an ingredient to the list of ingredients needed to make a given recipe
-//    public void addIngredientToRecipe() {
-//        //stub
-//    }
-//
-//    public String getRecipeName() {
-//        return this.name;
-//    }
-//
-//    public int getPrepTime() {
-//        return this.prepTime;
-//    }
-//
-//    public List<Ingredient> getIngredientsNeeded() {
-//        return this.ingredientsNeeded;
-//    }
+    private final String name;
+    private List<Ingredient> recipeIngredients;
+    private final int prepTime;
+
+    // REQUIRES: recipeName has a non-zero length, time > 0.
+    // EFFECTS: creates a new Recipe with a given name and prep time (in minutes),
+    // and an empty list of ingredients required.
+    public Recipe(String recipeName, int time) {
+        this.name = recipeName;
+        this.prepTime = time;
+        this.recipeIngredients = new ArrayList<>();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: return true if ingredient has not already been added to recipe and add ingredient to recipe,
+    // or return false.
+    public Boolean addIngredientToRecipe(Ingredient i) {
+        for (Ingredient ingredient : this.recipeIngredients) {
+            if (Objects.equals(ingredient.getIngredientName(), i.getIngredientName())) {
+                return false;
+            }
+        }
+
+        this.recipeIngredients.add(i);
+        return true;
+
+    }
+
+    public String getRecipeName() {
+        return this.name;
+    }
+
+    public int getPrepTime() {
+        return this.prepTime;
+    }
+
+    public List<Ingredient> getRecipeIngredients() {
+        return this.recipeIngredients;
+    }
 }
--- src/test/model/RecipeListTest.java
+++ src/test/model/RecipeListTest.java
@@ -2,16 +2,62 @@ package model;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class RecipeListTest {
-//    private Recipe testRecipe;
-//    private Ingredient i1;
-//    private Ingredient i2;
-//    private Ingredient i3;
-//
-//    @BeforeEach
-//    void runBefore() {
-//        testRecipe = new Recipe("Fried Chicken", 30);
-//    }
+    private Recipe testRecipe;
+    private Ingredient i1;
+    private Ingredient i2;
+    private Ingredient i3;
+
+    @BeforeEach
+    void runBefore() {
+        testRecipe = new Recipe("Fried Chicken", 30);
+        i1 = new Ingredient("Chicken");
+        i2 = new Ingredient("Oil");
+        i3 = new Ingredient("Salt");
+    }
+
+    @Test
+    void testConstructor() {
+        assertEquals("Fried Chicken", testRecipe.getRecipeName());
+        assertEquals(30, testRecipe.getPrepTime());
+        List<Ingredient> ingredientsNeeded = testRecipe.getRecipeIngredients();
+        assertEquals(0, ingredientsNeeded.size());
+    }
+
+    @Test
+    void testAddOneIngredient() {
+        assertTrue(testRecipe.addIngredientToRecipe(i1));
+        List<Ingredient> ingredientsNeeded = testRecipe.getRecipeIngredients();
+        assertEquals(1, ingredientsNeeded.size());
+        assertEquals(i1, ingredientsNeeded.get(0));
+    }
+
+    @Test
+    void testAddMultipleIngredients() {
+        assertTrue(testRecipe.addIngredientToRecipe(i1));
+        assertTrue(testRecipe.addIngredientToRecipe(i2));
+        assertTrue(testRecipe.addIngredientToRecipe(i3));
+        List<Ingredient> ingredientsNeeded = testRecipe.getRecipeIngredients();
+        assertEquals(3, ingredientsNeeded.size());
+        assertEquals(i1, ingredientsNeeded.get(0));
+        assertEquals(i2, ingredientsNeeded.get(1));
+        assertEquals(i3, ingredientsNeeded.get(2));
+    }
+
+    @Test
+    void testAddDuplicateIngredients() {
+        assertTrue(testRecipe.addIngredientToRecipe(i1));
+        assertFalse(testRecipe.addIngredientToRecipe(i1));
+        assertTrue(testRecipe.addIngredientToRecipe(i2));
+        List<Ingredient> ingredientsNeeded = testRecipe.getRecipeIngredients();
+        assertEquals(2, ingredientsNeeded.size());
+        assertEquals(i1, ingredientsNeeded.get(0));
+        assertEquals(i2, ingredientsNeeded.get(1));
+    }
+
 }
